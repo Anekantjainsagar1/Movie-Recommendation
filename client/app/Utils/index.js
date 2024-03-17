178422dbb5b8ecd411bf0f9eb743549139d08f61
Anekantjainsagar1 <jainanekantsagar@gmail.com>
2024-03-17 18:27:35 +0530

    Updated the url

--- client/app/Utils/index.js
+++ client/app/Utils/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000'
-// export const BASE_URL = "https://movie-recommendation-3xk5.onrender.com";
+// export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = "https://movie-recommendation-3xk5.onrender.com";
